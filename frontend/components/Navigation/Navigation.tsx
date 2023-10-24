c46cd565bc2d7db8ac9ef5eed4e3f142e31f29ea
azkuang <alexzkuang0314@gmail.com>
2023-10-24 13:39:49 -0400

    Added About Us, Resources, and Contact pages.

--- frontend/components/Navigation/Navigation.tsx
+++ frontend/components/Navigation/Navigation.tsx
@@ -6,6 +6,9 @@ const Navigation = () => {
 	return (
 		<nav className={styles.navbar}>
 			<Link href='/'>Home</Link>
+			<a href="/1">About us</a>
+			<a href="/2">Resources</a>
+			<a href="/3">Contact</a>
 		</nav>
 	);
 };
